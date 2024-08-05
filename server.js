639792d092ed68b50d3cb4c84469ca3cd85883d0
Vishrut Golakiya <101960778+vishrut-golakiya@users.noreply.github.com>
2024-08-05 14:20:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('this is just a demo for ci checking');
+  res.end('this is just a demo for  checking');
 });
 
 server.listen(port, hostname, () => {
